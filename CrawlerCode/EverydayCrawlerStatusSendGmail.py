8755f01d229c24449d3da3cef2877b00e67d7502
f496328mm <f496328mm@gmail.com>
2018-05-26 16:47:24 +0800

    sendgmail

---
+++ CrawlerCode/EverydayCrawlerStatusSendGmail.py
@@ -0,0 +1,150 @@
+
+
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+import time
+import datetime
+
+os.chdir('/home/linsam/github/FinancialMining/CrawlerCode')
+sys.path.append('/home/linsam/github/FinancialMining/CrawlerCode')
+import load_data
+
+#----------------------------------------------------------------------------
+# self = EverydayCrawlerStatus()
+class EverydayCrawlerStatus:
+    def __init__(self):
+        self.host = load_data.host
+        self.user = load_data.user
+        self.password = load_data.password
+        self.database = 'python'
+        
+        tem = load_data.execute_sql2(self.host,self.user,self.password,self.database,'SHOW TABLES')
+        tem = np.concatenate(tem, axis=0)
+        self.datatable = [ te for te in tem ]
+        self.datatable.remove('new')
+
+    def CrawlerStatus(self):
+        self.cdate = pd.DataFrame()
+        for dt in self.datatable:
+            text = "SELECT name,CrawlerDate FROM `" + dt + "` where id = ( SELECT max(id) FROM `" + dt + "` )"
+            tem = load_data.execute_sql2(self.host,
+                                         self.user,
+                                         self.password,
+                                         self.database,
+                                         text)
+            tem = pd.DataFrame( np.concatenate(tem, axis=0) )
+            self.cdate = self.cdate.append(tem.T)
+        
+        self.cdate.index = range(len(self.cdate))
+        self.cdate.columns = ['name','date']
+
+    def change_status(self):
+        #self.CrawlerStatus()
+        
+        self.cdate['status'] = ''
+        today = str( datetime.datetime.now() ).split(' ')[0]
+        for i in range(len(self.cdate)):
+            tem = str( self.cdate['date'][i] ).split(' ')[0]
+            
+            if tem == today :
+                self.cdate['status'][i] = 'ok'
+            
+            
+            
+#----------------------------------------------------------------------------    
+# self = SendGmail('123')
+class SendGmail:
+    def __init__(self,cdate):
+        self.driver = webdriver.Firefox()
+        url = 'https://mail.google.com/mail/?tab=wm'
+        self.driver.get(url)
+        #self.title = title
+        self.cdate = cdate
+    
+    #-------------------------------------------------------------
+    def main(self):
+        def create_title(cdate):
+            
+            ok_sum = np.sum( cdate['status'] == 'ok' )
+            if ok_sum == len(cdate):
+                title = 'ok'
+            return title
+        
+        def create_text(cdate):
+            text = ''
+            for i in range(len(cdate)):
+                text = text + cdate['name'][i] + ' : ' + str( cdate['date'][i] ) + '\n'
+            return text
+        #-------------------------------------------------------------
+        bo = 1
+        while(bo):
+            try:
+                self.driver.find_element_by_id('identifierId').send_keys('samlin266118@gmail.com')
+        
+                self.driver.find_element_by_id('identifierNext').click()
+                bo = 0
+            except:
+                bo = 1        
+        #-------------------------------------------------------------
+        bo = 1
+        while(bo):
+            try:
+                self.driver.find_element_by_name('password').send_keys('XUP6Y7VM08')
+                
+                self.driver.find_element_by_id('passwordNext').click()
+                bo = 0
+            except:
+                bo = 1
+        #-------------------------------------------------------------
+        self.driver.find_element_by_id('passwordNext').click()
+        time.sleep(1)
+        # go to send email
+        url = 'https://mail.google.com/mail/u/0/?tab=wm#inbox?compose=new'
+        self.driver.get(url)
+        # key to someone email
+        time.sleep(1)
+        self.driver.find_element_by_name('to').send_keys('samlin266118@gmail.com')
+        
+        # key title
+        title = create_title(self.cdate)
+        self.driver.find_element_by_name('subjectbox').send_keys(title)# title
+        self.driver.find_element_by_class_name('Ap').click()
+        
+        # key article text
+        text = create_text(self.cdate)
+        self.driver.find_element_by_class_name('Ap').send_keys(text)# text
+        
+        # click send email
+        button = self.driver.find_elements_by_xpath('//*[@role="button"]')
+        button[44].click()
+        time.sleep(2)
+        # quit
+        self.driver.quit()
+        
+        
+def main():
+    
+    ECS = EverydayCrawlerStatus()
+    ECS.CrawlerStatus()
+    ECS.change_status()
+    
+    SG = SendGmail(ECS.cdate)
+    SG.main()
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
